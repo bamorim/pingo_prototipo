10a35bb0c140659cbd28f23ca86921e8ab425d56
Bernardo Amorim <contato@bamorim.com>
2016-07-21 10:04:46 -0300

    Unique username

--- priv/repo/migrations/20160720235506_create_user.exs
+++ priv/repo/migrations/20160720235506_create_user.exs
@@ -3,11 +3,12 @@ defmodule Pingo.Repo.Migrations.CreateUser do
 
   def change do
     create table(:users) do
-      add :username, :string, unique: true
+      add :username, :string
       add :encrypted_password, :string
 
       timestamps()
     end
 
+    create unique_index(:users, [:username])
   end
 end
